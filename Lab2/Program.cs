cf93bd1f98b2b28211343fb52959ca6455363db2
Aaron <atsand23@gmail.com>
2018-01-24 10:09:46 -0500

    cleaned up

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -12,7 +12,6 @@ namespace Lab2
         {
             int input;
             string name = "Ricky Bobbie", realName;
-            bool check = false;
             bool again = true;
             string answer;
 
@@ -22,12 +21,9 @@ namespace Lab2
             Console.WriteLine();
             while (again)
             {
-                check = false;
-                Console.WriteLine(name + ", please enter an intiger between 1-100.", name);
+                Console.WriteLine(name + ", please enter an integer between 1-100.");
                 input = int.Parse(Console.ReadLine());
 
-                while (check == false)
-                {
                     if (input < 1 || input > 100)
                     {
                         Console.WriteLine("Sorry, {0}, but that number is out of range.  Please enter a different number between 1-100.", name);
@@ -36,30 +32,26 @@ namespace Lab2
                     else if ((input % 2) != 0)
                     {
                         Console.WriteLine(name + ", your input of " + input + " is an odd number.");
-                        check = true;
                     }
                     else if (input >= 2 && input < 25)
                     {
                         Console.WriteLine(name + ", your input is even and less than 25.");
-                        check = true;
                     }
                     else if (input >= 26 && input <= 60)
                     {
                         Console.WriteLine(name + ", your input of is even.");
-                        check = true;
                     }
                     else
                     {
                         Console.WriteLine(name + ", your input of " + input + " is even.");
-                        check = true;
                     }
-                }
                          
                 Console.WriteLine(name + ", would you like to check another number? (Y/N)");
                 answer = Console.ReadLine();
                 if (answer!="y" && answer!="Y")
                 {
                     again = false;
+                    Console.WriteLine("Goodbye!");
                 }
             }
         }
